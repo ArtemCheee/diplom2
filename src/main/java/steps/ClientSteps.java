9e51328cd4c9490d8e97fed45138d44220310b0f
Chernoschekov Artem <solo16394@mail.ru>
2026-06-24 14:54:11 +0600

    Шаги, дергаю ручки

---
+++ src/main/java/steps/ClientSteps.java
@@ -0,0 +1,45 @@
+package steps;
+
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+import io.restassured.http.ContentType;
+import model.ClientModel;
+
+
+import static data.ClientData.CREATE_CLIENT_PATH;
+
+
+import static data.ClientData.DELETE_CLIENT_PATH;
+import static io.restassured.RestAssured.given;
+
+public class ClientSteps {
+
+    @Step("Создание клиента")
+    public static Response createClient(ClientModel client) {
+        return given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(client)
+                .when()
+                .post(CREATE_CLIENT_PATH)
+                .then()
+                .extract().response();
+
+    }
+
+
+    @Step("Удаление клиента")
+
+    public static Response deleteClient(String accessToken) {
+        return given()
+                .log().all()
+                .header("Authorization", accessToken)
+                .when()
+                .delete(DELETE_CLIENT_PATH)
+                .then()
+                .log().all()
+                .extract().response();
+
+    }
+
+}
