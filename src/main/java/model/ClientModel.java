697f0456b7b8591e461af9352800363f18c15003
Chernoschekov Artem <solo16394@mail.ru>
2026-06-24 14:53:22 +0600

    Конструкторы пустой и полный, геттеры, сеттеры

--- src/main/java/model/ClientModel.java
+++ src/main/java/model/ClientModel.java
@@ -1,12 +1,16 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 
 public class ClientModel {
     private String email;
